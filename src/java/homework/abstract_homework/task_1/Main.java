157aadf3a22d7fca4b7f013a6c1c5f1dafd99f13
denismikhno <80958751+denismikhno@users.noreply.github.com>
2025-04-01 15:00:54 +0300

    Revert "Выполнил задание homework.abstract_homework.task_1"

--- src/java/homework/abstract_homework/task_1/Main.java
+++
@@ -1,9 +0,0 @@
-package homework.abstract_homework.task_1;
-
-public class Main {
-    //static Logger logger = new FileLogger();
-    static Logger logger = new ConsoleLogger();
-    public static void main(String[] args) {
-        logger.logMessage("Пишу свои логи!");
-    }
-}
