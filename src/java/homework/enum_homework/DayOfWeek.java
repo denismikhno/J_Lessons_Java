c8cae6244d387a87a0333f9703cff78643a2ebeb
denismikhno_laptop <denismikhno@gmail.com>
2025-04-02 16:09:53 +0300

    Выполнил не полностью задание homework.enum_homework. Доработать красивый вывод списка дел.

--- src/java/homework/enum_homework/DayOfWeek.java
+++ src/java/homework/enum_homework/DayOfWeek.java
@@ -1,5 +1,22 @@
 package homework.enum_homework;
 
 public enum DayOfWeek {
-    MONDAY
+    MONDAY("Понедельник"),
+    TUESDAY("Вторник"),
+    WEDNESDAY("Среда"),
+    THURSDAY("Четверг"),
+    FRIDAY("Пятница"),
+    SATURDAY("Суббота"),
+    SUNDEY("Воскресенье");
+
+    String day;
+
+    DayOfWeek(String day) {
+        this.day = day;
+    }
+
+    @Override
+    public String toString() {
+        return day;
+    }
 }
--- src/java/homework/enum_homework/Schedule.java
+++ src/java/homework/enum_homework/Schedule.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Schedule {
-    Map<DayOfWeek, List<String>> schedule = new HashMap<>();
+    Map<DayOfWeek, List<Task>> schedule = new HashMap<>();
 
     public Schedule() {
         for(DayOfWeek dayOfWeek : DayOfWeek.values()) {
@@ -14,9 +14,11 @@ public class Schedule {
         }
     }
 
-    void addTask(DayOfWeek day, String task) {
-        List<String> daySchedule = schedule.get(day);
-        daySchedule.add(task);
+    void addTask(Task... newTask) {
+        for (Task task : newTask) {
+            List<Task> daySchedule = schedule.get(task.getTaskAssignmentDay());
+            daySchedule.add(task);
+        }
     }
 
     void printSchedule() {
@@ -25,13 +27,19 @@ public class Schedule {
 
     public static void main(String[] args) {
         Schedule schedule = new Schedule();
+        Task task1 = new Task("Почистить зубы", DayOfWeek.MONDAY,30, "не выполнено");
+        Task task2 = new Task("Умыться", DayOfWeek.TUESDAY,30, "не выполнено");
+        Task task3 = new Task("Поесть", DayOfWeek.WEDNESDAY,30, "не выполнено");
 
-        DayOfWeek monday = DayOfWeek.MONDAY;
-        String mondayTask = "почистить зубы";
-        String mondayTask2 = "почистить картошку";
+        Task task4 = new Task("Проспать работу", DayOfWeek.THURSDAY,0, "выполнено");
+        Task task5 = new Task("Сходить в магазин", DayOfWeek.FRIDAY,60, "не выполнено");
+        Task task6 = new Task("Отремонтировать машину", DayOfWeek.SATURDAY,120, "не выполнено");
 
-        schedule.addTask(monday, mondayTask);
-        schedule.addTask(monday, mondayTask2);
+        Task task7 = new Task("Приготовить есть", DayOfWeek.SUNDEY,60, "не выполнено");
+        Task task8 = new Task("Смотреть телевизор", DayOfWeek.MONDAY,120, "выполнено");
+        Task task9 = new Task("Погладить вещи", DayOfWeek.MONDAY,30, "не выполнено");
+
+        schedule.addTask(task1, task2, task3, task4, task5, task6, task7, task8, task9);
 
         schedule.printSchedule();
     }
