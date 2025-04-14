985d8c165c473814f2e78d7e17cd9e1baf2876b0
denismikhno_laptop <denismikhno@gmail.com>
2025-04-14 14:11:37 +0300

    Выполнил classes_homework.task_3

---
+++ src/java/homework/classes_homework/task_3/Fish.java
@@ -0,0 +1,10 @@
+package homework.classes_homework.task_3;
+
+public class Fish extends Animal {
+    private int sizeInFt;
+    private Boolean canEat;
+
+    private void swim() {
+        System.out.println("Рыба плывет");
+    }
+}
---
+++ src/java/homework/classes_homework/task_3/Zebra.java
@@ -0,0 +1,9 @@
+package homework.classes_homework.task_3;
+
+public class Zebra extends Animal {
+    public Boolean is_wild;
+
+    public void run() {
+        System.out.println("Зебра бежит");
+    }
+}
