7fb1696602ca86eefb020b1a32c3a3163c8f4538
denismikhno <80958751+denismikhno@users.noreply.github.com>
2025-04-14 14:22:27 +0300

    Revert "Выполнил homework.classes_homework.task_3"

--- src/java/homework/classes_homework/task_3/Zebra.java
+++
@@ -1,9 +0,0 @@
-package homework.classes_homework.task_3;
-
-public class Zebra extends Animal {
-    public Boolean is_wild;
-
-    public void run() {
-        System.out.println("Зебра бежит");
-    }
-}
